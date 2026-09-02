7b589aba02e35fc30b85b895e6e07ca9e0baeace
Jacob Bachrach <jbachrach45@gmail.com>
2026-09-02 17:01:08 -0400

    "Added up to the 7th test case scenario. Check the comment on the second scenario."

--- app/src/test/java/org/dgib/EmployeeTest.java
+++ app/src/test/java/org/dgib/EmployeeTest.java
@@ -23,4 +23,72 @@ public class EmployeeTest {
         double actualPay = emp.getPay();
         assertEquals(expectedPay, actualPay);
     }
+
+    @Test
+    @DisplayName("Worked 12 hours on 4 week days")
+    void testGetPay_under40_4WDW_0WEDW() {
+        Employee emp = new Employee("emp",10.0);
+        emp.setHours(0, 3);
+        emp.setHours(1, 1);
+        emp.setHours(3, 3);
+        emp.setHours(4, 5);
+        double expectedPay = 120.0;
+        double actualPay = emp.getPay();
+        assertEquals(expectedPay, actualPay);
+    }
+
+    @Test
+    @DisplayName("Worked 41 hours on 4 week days")
+    void testGetPay_over40_4WDW_0WEDW() {
+        Employee emp = new Employee("emp",10.0);
+        emp.setHours(0, 10);
+        emp.setHours(1, 10);
+        emp.setHours(2, 10);
+        emp.setHours(3, 11);
+        double expectedPay = 415.0;
+        double actualPay = emp.getPay();
+        assertEquals(expectedPay, actualPay);
+    }
+
+    @Test
+    @DisplayName("Worked 20 hours on 4 week days, 5 hours on the weekend")
+    void testGetPay_under40_4WDW_1WEDW() {
+        Employee emp = new Employee("emp",10.0);
+        emp.setHours(0, 5);
+        emp.setHours(2, 5);
+        emp.setHours(3, 5);
+        emp.setHours(4, 5);
+        emp.setHours(5, 5);
+        double expectedPay = 300.0;
+        double actualPay = emp.getPay();
+        assertEquals(expectedPay, actualPay);
+    }
+
+    @Test
+    @DisplayName("Worked 40 hours on 4 week days, 11 hours on the weekend")
+    void testGetPay_over40_4WDW_1WEDW() {
+        Employee emp = new Employee("emp",10.0);
+        emp.setHours(1, 10);
+        emp.setHours(2, 10);
+        emp.setHours(3, 10);
+        emp.setHours(4, 10);
+        emp.setHours(5, 11);
+        double expectedPay = 620.0;
+        double actualPay = emp.getPay();
+        assertEquals(expectedPay, actualPay);
+    }
+
+    @Test
+    @DisplayName("Worked 39 hours on 5 week days")
+    void testGetPay_under40_5WDW_0WEDW() {
+        Employee emp = new Employee("emp",10.0);
+        emp.setHours(0, 7);
+        emp.setHours(1, 8);
+        emp.setHours(2, 8);
+        emp.setHours(3, 8);
+        emp.setHours(4, 8);
+        double expectedPay = 390.0;
+        double actualPay = emp.getPay();
+        assertEquals(expectedPay, actualPay);
+    }
 }
